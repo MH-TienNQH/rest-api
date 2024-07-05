63066f4e50e84fd1008615091dc540377067bd9a
MH-TienNQH <tien.nqh@mor.com.vn>
2024-07-05 17:08:43 +0700

    added mist

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ import express from "express"
 import dotenv from "dotenv"
 
 dotenv.config()
+const PORT = process.env.PORT;
 
 const app = express();
 
-const PORT = process.env.PORT;
+app.get("/", (req, res, next) => {
+    res.send({message: "ok"})
+})
 
-app.listen(PORT, () => `Running on http://localhost:${PORT}`);
+app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
